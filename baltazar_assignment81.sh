8b67a269465ce51f2658ea59de716729da0bde2f
ReymondB01 <89466461+ReymondB01@users.noreply.github.com>
2021-10-08 10:52:58 +0800

    Add files via upload

--- baltazar_assignment81.sh
+++ baltazar_assignment81.sh
@@ -1,27 +1,98 @@
 #!/bin/bash
 
-echo ""
+echo "###############################"
+echo "#                             #"
+echo "#      Installing httpd       #"
+echo "#                             #"
+echo "###############################"
 yum install httpd -y
 
-echo ""
+echo "###################################################"
+echo "#                                                 #"
+echo "#       Starting and Enabling Apache server       #"
+echo "#                                                 #"
+echo "###################################################"
 systemctl start httpd
 systemctl enable httpd
 
-echo ""
+echo "###########################################################################"
+echo "#                                                                         #"
+echo "#        Adding the Firewall permissions for port 80 and port 443         #"
+echo "#                                                                         #"
+echo "###########################################################################"
 firewall-cmd --permanent --add-port=80/tcp
 firewall-cmd --permanent --add-port=443/tcp
 
-echo ""
+echo "###################################################"
+echo "#                                                 #"
+echo "#        Starting to reload the firewall          #"
+echo "#                                                 #"
+echo "###################################################"
 firewall-cmd --reload
 
-echo ""
+echo "###################################################"
+echo "#                                                 #"
+echo "#         Installing php and php module           #"
+echo "#                                                 #"
+echo "###################################################"
 yum install php php-mysql -y
-
-echo ""
 yum install php-fpm -y
 
-echo ""
+echo "###########################################################"
+echo "#                                                         #"
+echo "#       Restarting the Apache server in T -1 second       #"
+echo "#                                                         #"
+echo "###########################################################"
 systemctl restart httpd
 
-echo ""
-echo "<?php phpinfo(); ?>" > /var/www/html/info.php
+echo "###################################"
+echo "#                                 #"
+echo "#      The web Server is up!      #"
+echo "#                                 #"
+echo "###################################"
+
+echo "<?php phpinfo(); ?>" > /var/www/html/index.php
+
+echo "###################################################"
+echo "#                                                 #"
+echo "#      Installing mariadb-server and mariadb      #"
+echo "#                                                 #"
+echo "###################################################"
+yum install mariadb-server mariadb -y
+systemctl start mariadb
+systemctl enable mariadb
+
+echo "################################################"
+echo "#                                              #"
+echo "#       Secure Installation Proccessing...     #"
+echo "#                                              #"
+echo "################################################"
+
+mysql_secure_installation <<HERE
+
+y
+Babymj3214
+Babymj3214
+y
+y
+y
+y
+HERE
+
+echo "#######################################################"
+echo "#                                                     #"
+echo "#      The installation of a LAMP Stack is done!      #"
+echo "#                                                     #"
+echo "#######################################################"
+
+
+echo "#######################################################"
+echo "#                                                     #"
+echo "#            #######    #######    #####              #"
+echo "#            #     #       #       #    #             #"
+echo "#            #######       #       #####              #"
+echo "#            # #           #       #    #             #"
+echo "#            #   #         #       #    #             #"
+echo "#            #     #       #       #####              #"
+echo "#                                                     #"
+echo "#######################################################"
